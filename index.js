757476f0b41cd2eea431270bc6241aa4ab556571
EoBryanDev <mauriciobryan1998@gmail.com>
2022-05-31 20:08:41 -0300

    Filter by Category

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 const csvtojson = require('csvtojson')
 const fs = require('fs')
 
-const csvfilepath = "./Relatorio_cadop.csv"
+const csvfilepath = "./Relatorio_cadop_comId.csv"
 
 csvtojson()
 .fromFile(csvfilepath)
 .then((json)=>{
     console.log(json);
-    fs.writeFileSync("db.json", JSON.stringify(json),'utf-8',(err) => {
+    fs.writeFileSync("db.json3", JSON.stringify(json),'utf-8',(err) => {
         if(err) console.log(err)
     })
 })
